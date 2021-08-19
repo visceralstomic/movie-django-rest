cd0984b865a3cac5d7cc9465797210e2523cd968
visceralstomic <forgitpurposeonly@gmail.com>
2021-08-19 21:43:33 +0300

    Swagger doc

--- movie_site/movie_site/urls.py
+++ movie_site/movie_site/urls.py
@@ -1,20 +1,24 @@
-"""movie_site URL Configuration
 
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path, include
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+from rest_framework.permissions import AllowAny
+
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Movies API",
+      default_version='v1',
+      description="Movies site endpoints",
+      terms_of_service="https://www.google.com/policies/terms/",
+      contact=openapi.Contact(email="contact@snippets.local"),
+      license=openapi.License(name="BSD License"),
+   ),
+   public=True,
+   permission_classes=(AllowAny,),
+)
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -22,4 +26,6 @@ urlpatterns = [
     path('users/', include('users_app.urls')),
    	path('api-auth/', include('rest_framework.urls')),
     path('', include('frontend.urls')),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
